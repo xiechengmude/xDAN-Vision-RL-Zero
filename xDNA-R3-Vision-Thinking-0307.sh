f325089e985053c5cd7f9ba989def58294b617bc
shootime2021 <13760404575@163.com>
2025-03-07 19:56:41 +0800

    add fix

--- xDNA-R3-Vision-Thinking-0307.sh
+++ xDNA-R3-Vision-Thinking-0307.sh
@@ -12,11 +12,11 @@ mkdir -p "${SAVE_PATH}/${MODEL_CPK_NAME}/logs"
 
 
 ray job submit --address="http://127.0.0.1:8265" \
-   --runtime-env-json='{"working_dir": "/openrlhf"}' \
+   --runtime-env-json='{"working_dir": "/data/vayu/train/xDAN-Vision-RL-Zero", "env_vars": {"MASTER_ADDR": "10.11.50.36", "MASTER_PORT": "24999"}}' \
    -- python3 -m openrlhf.cli.train_ppo_ray \
    --ref_num_nodes 1 \
    --ref_num_gpus_per_node 8 \
-   --remote_rm_url /openrlhf/examples/scripts/math_verifier.py \
+   --remote_rm_url examples/scripts/math_verifier.py \
    --actor_num_nodes 2 \
    --actor_num_gpus_per_node 8 \
    --vllm_num_engines 1 \
@@ -31,7 +31,7 @@ ray job submit --address="http://127.0.0.1:8265" \
    --micro_train_batch_size 1 \
    --train_batch_size 16 \
    --micro_rollout_batch_size 1 \
-   --rollout_batch_size 64 \
+   --rollout_batch_size 32 \
    --temperature 1 \
    --n_samples_per_prompt 8 \
    --max_epochs 1 \

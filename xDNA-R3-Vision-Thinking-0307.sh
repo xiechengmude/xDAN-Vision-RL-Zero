665e541d657fca16dadfa7de2bcad4658239f6b2
shootime2021 <13760404575@163.com>
2025-03-08 14:01:20 +0800

    cancel xDNA-R3-Vision-Thinking-0308.log

--- xDNA-R3-Vision-Thinking-0307.sh
+++ xDNA-R3-Vision-Thinking-0307.sh
@@ -40,7 +40,6 @@ ray job submit --address="http://127.0.0.1:8265" \
    --advantage_estimator reinforce_baseline \
    --zero_stage 3 \
    --adam_offload \
-   --adam_betas 0.9,0.999 \
    --bf16 \
    --actor_learning_rate 1e-6 \
    --init_kl_coef 0.02 \
